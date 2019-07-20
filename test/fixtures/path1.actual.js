e22c7184d5e1eba5400d2c80f842dfb178130aa1
mattdesl <dave.des@gmail.com>
2019-07-20 10:58:02 +0100

    use arguments not concat for path

--- test/fixtures/path1.actual.js
+++ test/fixtures/path1.actual.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path');
-const str = fs.readFileSync(path.join(__dirname + '/hello.txt'), 'utf8');
+const str = fs.readFileSync(path.join(__dirname, '/hello.txt'), 'utf8');
 console.log(str);
--- test/fixtures/path2.actual.js
+++ test/fixtures/path2.actual.js
@@ -1,4 +1,4 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-const str = readFileSync(join(__dirname + '/hello.txt'), 'utf8');
+const str = readFileSync(join(__dirname, '/hello.txt'), 'utf8');
 console.log(str);
